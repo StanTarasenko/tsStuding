d3129108464d5d8fd8b94521200ed15ec0d49570
Stan Tarasenko <162692442+StanislawTarasenko@users.noreply.github.com>
2024-10-21 20:39:28 +0300

    final version

--- src/homework10.ts
+++ src/homework10.ts
@@ -1,21 +1,34 @@
 // DeepReadonly
-type DeepReadonly = {
-    a: {
-      b: {
-        c: string;
-      };
-    };
-    d: number[];
-    e: () => void;
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends object 
+    ? T[K] extends Function 
+      ? T[K] 
+      : DeepReadonly<T[K]> 
+    : T[K];
 };
 
 // DeepRequireReadonly
-type Example = {
-    a?: {
-      b?: {
-        c?: string;
-      };
-    };
-    d?: number[];
-    e: () => void;
+type DeepRequireReadonly<T> = {
+  readonly [K in keyof T]-?: T[K] extends object 
+    ? T[K] extends Function 
+      ? T[K] 
+      : DeepRequireReadonly<T[K]> 
+    : T[K];
+};
+
+// UpperCaseKeys
+type UpperCaseKeys<T> = {
+  [K in keyof T as Uppercase<K & string>]: T[K];
+};
+
+// ObjectToPropertyDescriptor
+type CustomPropertyDescriptor<T> = {
+  value: T;
+  writable?: boolean;
+  enumerable?: boolean;
+  configurable?: boolean;
+};
+
+type ObjectToPropertyDescriptor<T> = {
+  [K in keyof T]: CustomPropertyDescriptor<T[K]>;
 };
